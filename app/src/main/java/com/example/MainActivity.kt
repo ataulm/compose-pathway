0366d2e53ed35c5d3fd37d0d14a88b1af98175d3
Jacek Szmelter <jacek.szmelter.ext@qonto.com>
2021-05-07 16:56:19 +0200

    Add animations to List.

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -12,8 +16,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,11 +30,6 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -39,33 +37,51 @@ fun DefaultPreview() {
 }
 
 @Composable
-fun Names(names: List<String>, modifier: Modifier = Modifier) {
+fun MainContent(names: List<String> = listOf("Android", "ios")) {
+    val counterState = rememberSaveable { mutableStateOf(0) }
+    Column(modifier = Modifier.fillMaxHeight()) {
+        Names(
+            modifier = Modifier.weight(1f),
+            names = List(1000) { "Hello Android $it" },
+            onClick = { counterState.value = counterState.value + 1 }
+        )
+        Counter(
+            count = counterState.value,
+            onClick = { counterState.value = counterState.value + 1 }
+        )
+    }
+}
+
+@Composable
+fun Names(names: List<String>, onClick: () -> Unit, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
         items(items = names) { name ->
-            Greeting(name = name)
+            Greeting(name = name, onClick = onClick)
             Divider(color = Color.Black)
         }
     }
 }
 
 @Composable
-fun MainContent(names: List<String> = listOf("Android", "ios")) {
-    val counterState = rememberSaveable { mutableStateOf(0) }
-    Column(modifier = Modifier.fillMaxHeight()) {
-        Names(modifier = Modifier.weight(1f), names = List(1000) {"Hello Android $it"})
-        Counter(
-            count = counterState.value,
-            onClick = {
-                counterState.value += 1
-            }
-        )
-    }
+fun Greeting(name: String, onClick: () -> Unit) {
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isSelected) Color.Red else Color.Transparent,
+        animationSpec = tween(1_000)
+    )
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Composable
 fun Counter(count: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
     Button(
-        modifier = modifier,
+        modifier = modifier.clickable { onClick() },
         onClick = { onClick() },
         colors = ButtonDefaults.buttonColors(
             backgroundColor = if (count > 5) Color.Green else Color.White
