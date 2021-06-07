651a80f1bd4cfbcc35676f09f9ab2bbfa706d5da
Ataul Munim <amunim@twitter.com>
2021-06-07 11:55:02 +0100

    Finish layouts.workingwithLists

--- layouts/src/main/java/com/example/layouts/ScaffoldActivity.kt
+++ layouts/src/main/java/com/example/layouts/ScaffoldActivity.kt
@@ -3,17 +3,25 @@ package com.example.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.layouts.ui.theme.LayoutsCodelabTheme
+import com.google.accompanist.coil.rememberCoilPainter
+import kotlinx.coroutines.launch
 
 class ScaffoldActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,8 +56,6 @@ fun LayoutsCodelab() {
             Text(text = "Drawer Item 1")
             Text(text = "Drawer Item 2")
         }
-
-
     ) { innerPadding ->
         val modifier = Modifier.padding(innerPadding)
         BodyContent(modifier)
@@ -58,11 +64,56 @@ fun LayoutsCodelab() {
 
 @Composable
 private fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        Row {
+            Button(
+                modifier = Modifier.weight(0.5f),
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                }) {
+                Text("Top")
+            }
+
+            Button(
+                modifier = Modifier.weight(0.5f),
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }) {
+                Text("Bottom")
+            }
+        }
+        LazyColumn(
+            modifier = modifier.fillMaxWidth(),
+            state = scrollState
+        ) {
+            items(listSize) { ImageListItem(it) }
+        }
     }
 }
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberCoilPainter(
+                request = "https://robohash.org/$index"
+            ),
+            contentDescription = "Robot $index",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+
+}
+
 
 @Preview
 @Composable
