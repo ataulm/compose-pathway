d4f9fd461cb0f50d77a274003207fdbcb42d68c3
jackszm <jszmelter@gmail.com>
2021-10-19 10:59:21 +0200

    In the middle of 10.4 Navigating with arguments.

--- android-compose-codelabs-main/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ android-compose-codelabs-main/NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -21,17 +21,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -51,13 +53,14 @@ class RallyActivity : ComponentActivity() {
 fun RallyApp() {
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
         val navController = rememberNavController()
+        val backStackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = RallyScreen.fromRoute(backStackEntry.value?.destination?.route)
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen -> navController.navigate(screen.name) },
                     currentScreen = currentScreen
                 )
             }
@@ -68,20 +71,32 @@ fun RallyApp() {
                 startDestination = RallyScreen.Overview.name,
             ) {
                 composable(RallyScreen.Overview.name) {
-//                    currentScreen.content(
-//                        onScreenChange = { screen ->
-//                            currentScreen = RallyScreen.valueOf(screen)
-//                        }
-//                    )
-                    Text(text = RallyScreen.Overview.name)
-                }
-
-                composable(RallyScreen.Accounts.name) {
-                    Text(RallyScreen.Accounts.name)
+                    OverviewBody(
+                        onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+                        onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
+                        onAccountClick = {
+                            // TODO
+                        }
+                    )
                 }
                 composable(RallyScreen.Bills.name) {
-                    Text(RallyScreen.Bills.name)
+                    BillsBody(bills = UserData.bills)
                 }
+                val accountsName = RallyScreen.Accounts.name
+                composable(
+                    route = accountsName
+                ) {
+                    AccountsBody(accounts = UserData.accounts)
+                }
+                composable(
+                    route = "$accountsName/{name}",
+                    arguments = listOf(
+                        navArgument("name") { type = NavType.StringType }
+                    )) {
+                    //  TODO : Open up Account Details Screen For a {name}
+                    /* The body of each composable destination receives */
+                }
+
             }
 
         }
